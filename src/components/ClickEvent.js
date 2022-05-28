6754ccc5f6b2fbb345619bd98c260cd1efabef03
MissDine <enidnyatichi1212@gmail.com>
2022-05-28 19:40:53 +0300

    Feat:Edit

--- src/components/ClickEvent.js
+++ src/components/ClickEvent.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 function ClickEvent() {
+    function clickHandler(){
+        console.log("Button clicked");
+    }
   return (
     <>
         <h1>Hi, Enid.</h1>
-        <button>Click here</button>
+        <button onClick ={clickHandler}>Click here</button>
     </>
   )
 }
