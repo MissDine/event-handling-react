6e8a53a3839738468be0a5108df93abdf5a4cea5
MissDine <enidnyatichi1212@gmail.com>
2022-05-28 18:43:42 +0300

    Feat:edit

---
+++ src/components/ClickEvent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ClickEvent() {
+  return (
+    <>
+        <h1>Hi, Enid.</h1>
+        <button>Click here</button>
+    </>
+  )
+}
+
+export default ClickEvent
